b05917943c642882b044ae522a2e33afe0c37656
Prabesh Sapkota <pspsapkota2059@gmail.com>
2023-04-07 20:43:59 +0545

    Update FakeAllPortsAsOpen.py

--- FakeAllPortsAsOpen.py
+++ FakeAllPortsAsOpen.py
@@ -1,9 +1,15 @@
-#!/usr/local/bin/python2
+#!/usr/bin/env python3
 from scapy.all import *
+import os
+
 os.system("iptables -A OUTPUT -p tcp -o wlan0 --sport 1:65535 --tcp-flags RST RST -j DROP")
+
 def packet(pkt):
     if pkt[TCP].flags == 2:
-        print('SYN packet detected port : ' + str(pkt[TCP].sport) + ' from IP Src : ' + pkt[IP].src)
+        print(f'SYN packet detected port : {str(pkt[TCP].sport)} from IP Src : {pkt[IP].src}')
         send(IP(dst=pkt[IP].src, src=pkt[IP].dst)/TCP(dport=pkt[TCP].sport, sport=pkt[TCP].dport,ack=pkt[TCP].seq + 1, flags='SA'))
-sniff(iface="wlan0", prn=packet, filter="tcp[0xd]&18=2",count=100)
+
+while True:
+    sniff(iface="wlan0", prn=packet, filter="tcp[0xd]&18=2", count=100)
+
 os.system("iptables -D OUTPUT -p tcp -o wlan0 --sport 1:65535 --tcp-flags RST RST -j DROP")
