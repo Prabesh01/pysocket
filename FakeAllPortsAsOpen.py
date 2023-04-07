cc000d014e8e8b93aae42a3a41e73a26e41da57e
Prabesh Sapkota <pspsapkota2059@gmail.com>
2023-04-07 20:49:29 +0545

    Update FakeAllPortsAsOpen.py

--- FakeAllPortsAsOpen.py
+++ FakeAllPortsAsOpen.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 from scapy.all import *
 import os
+import platform
 
-os.system("iptables -A OUTPUT -p tcp -o wlan0 --sport 1:65535 --tcp-flags RST RST -j DROP")
+if platform.system() == 'Windows':
+    os.system("netsh advfirewall firewall add rule name='Block RST packets' dir=out protocol=TCP remoteport=1-65535 tcpflags=RST action=block")
+else:
+    os.system("iptables -A OUTPUT -p tcp -o wlan0 --sport 1:65535 --tcp-flags RST RST -j DROP")
 
 def packet(pkt):
     if pkt[TCP].flags == 2:
@@ -12,4 +16,7 @@ def packet(pkt):
 while True:
     sniff(iface="wlan0", prn=packet, filter="tcp[0xd]&18=2", count=100)
 
-os.system("iptables -D OUTPUT -p tcp -o wlan0 --sport 1:65535 --tcp-flags RST RST -j DROP")
+if platform.system() == 'Windows':
+    os.system("netsh advfirewall firewall delete rule name='Block RST packets'")
+else:
+    os.system("iptables -D OUTPUT -p tcp -o wlan0 --sport 1:65535 --tcp-flags RST RST -j DROP")
